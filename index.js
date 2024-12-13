8227a04cc203fc10ff07b51725d76e62ea67e92c
asohyannick <asohyannick3@gmail.com>
2024-12-13 04:37:55 -0800

    Completed the comparison operator in JavaScript

---
+++ index.js
@@ -0,0 +1,42 @@
+// JavaScript Operators
+/*
+1.Arithmetic operator
+2.Comparison operator
+3.Bitwise operator
+4.Logical operator
+5.Assignment operator
+6.Special operator
+*/
+
+// Arithmetic operator
+
+let ab = 10;
+let bc = 3;
+let result = ab % bc;
+console.log(result);
+
+// Comparison 
+
+let a = 10;
+let b = 5;
+console.log(a == b);
+
+let c = 15;
+let d = 10;
+console.log(c === d);
+
+let cd = 10;
+let df = 5;
+console.log(cd != df);
+
+let abc = 10;
+let abcd = 5;
+console.log(abc > abcd);
+
+let m = 10;
+let n = 5;
+console.log(m >= 5);
+
+let y = 10;
+let x = 5;
+console.log(y <= x);
