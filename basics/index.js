094e4228781194e1df46784399a52e865af157e7
asohyannick <asohyannick3@gmail.com>
2024-12-13 04:15:01 -0800

    We have worked on the Arithmetic operator

--- basics/index.js
+++ basics/index.js
@@ -1,32 +1,39 @@
-// JS Data types : primitive and Reference Data Types
+// JavaScript Operators
 /*
- Primitive data types
- 1.String
- 2.Number
- 3.Boolean
- 4.Undefined
- 5.Null
- 6.Symbol
+1.Arithmetic operator
+2.Comparison operator
+3.Bitwise operator
+4.Logical operator
+5.Assignment operator
+6.Special operator
 */
 
-/*
-Reference Data Types
-1.Object
-2.Array
-*/
+// Arithmetic operator
 
+let a = 10;
+let b = 5;
+let result = a + b;  // addition operator  +
+console.log(result);
 
-let name = 'John';
-console.log(name);
-console.log(typeof(name));
-console.log(name.toLowerCase());
+let c = 10;
+let d = 5;
+let e = c - d; // substraction - operator
+console.log(e);
 
-let num1 = 10;
-console.log(num1 * 10);
+let ab = 10;
+let bc = 2;
+let cd = ab * bc; // Multiplication * operator
+console.log(cd);
 
+let m = 10;
+let n = 5;
+let mn = m / n;  // Division operator
+console.log(mn);
 
-let bool = false;
-console.log(bool);
+let ac = 10; // post increment in Js
+ac++;
+console.log(ac);
 
-let ab;
-console.log(ab);
+let ay = 10;
+--ay; // pre-decrement
+console.log(ay);
