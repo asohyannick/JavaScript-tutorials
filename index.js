0926da48746516f049dd1b1d45dc5dda27b092cc
asohyannick <asohyannick3@gmail.com>
2024-12-13 06:01:41 -0800

    Added the Logical operator sample code

--- index.js
+++ index.js
@@ -8,35 +8,50 @@
 6.Special operator
 */
 
-// Arithmetic operator
+// Logical Operator
+/*
 
-let ab = 10;
-let bc = 3;
-let result = ab % bc;
-console.log(result);
+1.Logical AND && operator
+ True: true && true return => true
+ False: true && false return => false
+ False: false && false return false
 
-// Comparison 
+ 2.Logical OR || operator
+ True: true || false = true;
+ False: false || false = true;
+ True: true || true = true;
+
+ 3.Logical NOT ! operator
+ False: !false to truthy
+ True: !true to falsy
+*/
 
 let a = 10;
 let b = 5;
-console.log(a == b);
+console.log(a > 5 && b > 2); // true
 
-let c = 15;
-let d = 10;
-console.log(c === d);
+let x = 10;
+let y = 5;
+console.log(x > 5 && y < 3); // false
 
-let cd = 10;
-let df = 5;
-console.log(cd != df);
+let i = 10;
+let j = 7;
+console.log(i > 20 && j > 10); // false
 
-let abc = 10;
-let abcd = 5;
-console.log(abc > abcd);
+let ab = 10;
+let cd = 5;
+console.log(ab > 5 || cd > 10); // true
 
 let m = 10;
 let n = 5;
-console.log(m >= 5);
+console.log(m > 20 || n > 10); // true
 
-let y = 10;
-let x = 5;
-console.log(y <= x);
+let abc = 20;
+let cd1 = 10;
+console.log(abc > 10 || cd1 > 5); // true
+
+let ab1 = 10;
+let cd2 = 5;
+console.log(ab1 !== cd2 ); // true
+
+
