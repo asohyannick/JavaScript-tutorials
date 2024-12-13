fe827f0113e529a95289406f90cd0d434fa424c0
asohyannick <asohyannick3@gmail.com>
2024-12-13 03:40:15 -0800

    Added more code on global variables

--- basics/index.js
+++ basics/index.js
@@ -1,47 +1,20 @@
-function greetTheWorld() {
-    alert("Hello World");
+// Global Variables
+// Global Variables can be declared outside a function scope or within the window object.
+// for example
+let value = 100; // global variable
+function globalvar() {
+    alert(value); 
+}
+globalvar();
+
+function newGlobe() {
+    window.value = 500; // global variable
 }
 
-// single line comment
-
-/*
-
-This is a multi-line comment
-*/
-
-
-// JavaScript Variables
-// let, const
-
-// primitive data types and reference data types
-let a = 5;
-let b = 10;
-let  c = b;
-let result = a + b;
-console.log(c);
-console.log(typeof(c));
-console.log(result);
-
-
-function add(num2) {
-    let num1 = 10; // local variable 
-    return num1 +  num2;
+function addGlobal() {
+    alert(window.value);
 }
 
-console.log(add(10));
+newGlobe();
+addGlobal();
 
-// global variable
-let num2 = 10;
-
-function sub(num3) {
-    return num2 + num3;
-}
-
-console.log(sub(5));
-
-
-/*
- JS Comments
- JS Variables
- JS Number Data types
-*/
