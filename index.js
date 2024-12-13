56522a37a9c6100b480d3cce3b1deecf8d2fdaba
asohyannick <asohyannick3@gmail.com>
2024-12-13 06:22:29 -0800

    Added more code sample on assignment operators in JavaScript

--- index.js
+++ index.js
@@ -8,50 +8,41 @@
 6.Special operator
 */
 
-// Logical Operator
+
 /*
-
-1.Logical AND && operator
- True: true && true return => true
- False: true && false return => false
- False: false && false return false
-
- 2.Logical OR || operator
- True: true || false = true;
- False: false || false = true;
- True: true || true = true;
-
- 3.Logical NOT ! operator
- False: !false to truthy
- True: !true to falsy
+Assignment operators 
+Assignment operators are used to assign values to variables in JavaScript. 
+They allow you to store data in a variable so that it can be used later in your code
+assign = operator
+add and  assign +=
+subtract and assign -=
+divide and assign /=
+multiple and assign *=
+modulus and assign %=
 */
 
 let a = 10;
-let b = 5;
-console.log(a > 5 && b > 2); // true
+let b = 10;
+let result = a + b;
+console.log(result);
 
-let x = 10;
-let y = 5;
-console.log(x > 5 && y < 3); // false
+let x = 5;
+x+= 20;
+console.log(x);
 
-let i = 10;
-let j = 7;
-console.log(i > 20 && j > 10); // false
+let y = 10;
+y-= 5;
+console.log(y);
 
-let ab = 10;
-let cd = 5;
-console.log(ab > 5 || cd > 10); // true
+let j = 20;
+j/= 2;
+console.log(j);
 
-let m = 10;
-let n = 5;
-console.log(m > 20 || n > 10); // true
+let k = 15;
+k*=2;
+console.log(k);
 
-let abc = 20;
-let cd1 = 10;
-console.log(abc > 10 || cd1 > 5); // true
-
-let ab1 = 10;
-let cd2 = 5;
-console.log(ab1 !== cd2 ); // true
-
+let p = 10;
+p%=3;
+console.log(p);
 
