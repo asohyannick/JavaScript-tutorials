bd5b44402af79fcb26c6d16b051d192c9b72aa38
asohyannick <asohyannick3@gmail.com>
2024-12-13 07:53:03 -0800

    I have added JavaScript Switch Statements

--- index.js
+++ index.js
@@ -1,53 +1,15 @@
-// JS If Statements
-/*
-1.If statement
-2.else if statement
-3.else statement
-*/
-
-const grade = 85;
-if(grade >= 90) {
-    console.log("Grade A");
-} else if(grade >= 87) {
-    console.log("Grade B");
-} else {
-    console.log("You're  doing your best.")
-}
-// Nested if statements
-
-let age = 20;
-let isStudent = true;
-let hasDrivingExperiences = 2;
-let hasGoodVision = true;
-let nonDrunkard = true;
-let hasDrivingLicense = true;
-let isMarried = true;
-
-if(age >= 18) {
-    if(isStudent) {
-        if(hasDrivingExperiences >= 1) {
-            if(hasGoodVision) {
-                if(nonDrunkard) {
-                    if(hasDrivingLicense) {
-                        console.log("You're qualify to drive.")
-                    } else {
-                        console.log("You're not eligible to drive.")
-                    }
-                }
-            }
-        }
-    }
-}
-
-
-// Using Logical Operators
-if(
-    (age >= 18 && hasDrivingLicense)
-     || (isStudent && hasDrivingExperiences >= 1)
-      || (hasGoodVision && nonDrunkard) || 
-      (hasDrivingLicense && isMarried)) {
-        console.log("You're eligible to drive.")
-
-} else {
-    console.log("Please buy a driving course online or get a student driving permit.")
+// JS Switch statements
+const fruit = 'apple';
+switch(fruit) {
+    case "bananas":
+        console.log("Cameroonian love eating bananas");
+        break;
+    case "oranges":
+        console.log("Oranges are good for our health");
+        break;
+    case "apple" :
+        console.log("Eating one apple per day keeps a doctor far from visiting you.")
+        break;
+    default:
+        console.log(`There's no ${fruit} right now in the fridge.`)
 }
