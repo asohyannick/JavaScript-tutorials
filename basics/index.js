3edaba5fb42ddac30a844e4a2eb698c190f134db
asohyannick <asohyannick3@gmail.com>
2024-12-13 03:52:57 -0800

    JavaScript Data Types have been added

--- basics/index.js
+++ basics/index.js
@@ -1,20 +1,32 @@
-// Global Variables
-// Global Variables can be declared outside a function scope or within the window object.
-// for example
-let value = 100; // global variable
-function globalvar() {
-    alert(value); 
-}
-globalvar();
+// JS Data types : primitive and Reference Data Types
+/*
+ Primitive data types
+ 1.String
+ 2.Number
+ 3.Boolean
+ 4.Undefined
+ 5.Null
+ 6.Symbol
+*/
 
-function newGlobe() {
-    window.value = 500; // global variable
-}
+/*
+Reference Data Types
+1.Object
+2.Array
+*/
 
-function addGlobal() {
-    alert(window.value);
-}
 
-newGlobe();
-addGlobal();
+let name = 'John';
+console.log(name);
+console.log(typeof(name));
+console.log(name.toLowerCase());
 
+let num1 = 10;
+console.log(num1 * 10);
+
+
+let bool = false;
+console.log(bool);
+
+let ab;
+console.log(ab);
