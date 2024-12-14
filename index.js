e0cdf409be03b0bd4e416de33dd7ac9ab4166144
asohyannick <asohyannick3@gmail.com>
2024-12-14 04:28:53 -0800

    Completed the while loop section of the course

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-// JS Switch statements
-const fruit = 'apple';
-switch(fruit) {
-    case "bananas":
-        console.log("Cameroonian love eating bananas");
-        break;
-    case "oranges":
-        console.log("Oranges are good for our health");
-        break;
-    case "apple" :
-        console.log("Eating one apple per day keeps a doctor far from visiting you.")
-        break;
-    default:
-        console.log(`There's no ${fruit} right now in the fridge.`)
+// JS Loops
+/*
+There are four types of loops in JavaScript
+1.for loop
+2.while loop
+3.do while loop
+4.for in loop
+*/
+
+// while loop
+
+let i = 0; // initialization point or initialize a variable
+
+while(true) { // condition to check if our  initialization is true
+    if(i >= 5) {
+        break; // stop the loop
+    }
+   console.log(i);
+   i++;
+}
+
+
+let j = 1;
+
+while( j <= 5) {
+    console.log(j);
+    j++;
 }
